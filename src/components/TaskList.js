fb03da7cd2dafac1e013100a722f8934cd3c910a
yomZsamora <samaurah@gmail.com>
2022-04-16 15:40:05 +0300

    Displaying each task using Task Component.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  console.log("MY tasks:");
+  console.log({tasks})
+
+  const myTasks = tasks.map((task, index) => (
+    <Task task={task} index={index} />
+  ))
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
-      <Task />
+      {myTasks}
     </div>
   );
 }
